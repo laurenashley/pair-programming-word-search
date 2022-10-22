785d0c08c61a8acdcc3b957d5bd31dfb4179f0b0
Lauren Ashley <la.zaubi@gmail.com>
2022-10-22 13:38:46 -0700

    pair programming with @Karamvir-Bains

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  let verticalJoin = [];
+  for (const i in letters[0]) {
+    let column = [];
+    for (const x in letters) {
+      column.push(letters[x][i]);
     }
-}
-
-module.exports = wordSearch
+    verticalJoin.push(column);
+  }
+  
+  verticalJoin = verticalJoin.map((ls) => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  
+  return false;
+};
+  
+module.exports = wordSearch;
